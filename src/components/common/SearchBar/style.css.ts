0410c7a5a11f0d1d700d9f523ef9e1596e0f094d
추성우 <cntjddn715@gmail.com>
2024-12-01 00:05:52 +0900

    chore :: SearchBar 컴포넌트

---
+++ src/components/common/SearchBar/style.css.ts
@@ -0,0 +1,17 @@
+import { font } from "@/ui/styles/font.css";
+import theme from "@/ui/styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const searchBar = style({
+  padding: "8px 16px",
+  display: "flex",
+  borderRadius: "4px",
+  border: `1px solid ${theme.gray[100]}`,
+  background: theme.white,
+  gap: "8px",
+  width: "360px",
+  ...font.p3,
+  "::placeholder": {
+    color: theme.gray[300],
+  },
+});
