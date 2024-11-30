a32bacabd8d5f3ec8e7e3f98bbaa25c924a5442c
추성우 <cntjddn715@gmail.com>
2024-12-01 01:02:51 +0900

    fix :: SearchBar input width 지정

--- src/components/common/SearchBar/index.tsx
+++ src/components/common/SearchBar/index.tsx
@@ -6,7 +6,11 @@ function SearchBar() {
   return (
     <div className={s.searchBar}>
       <Search />
-      <input type="text" placeholder="검색어를 입력해 주세요." />
+      <input
+        type="text"
+        placeholder="검색어를 입력해 주세요."
+        className={s.input}
+      />
     </div>
   );
 }
--- src/components/common/SearchBar/style.css.ts
+++ src/components/common/SearchBar/style.css.ts
@@ -5,12 +5,17 @@ import { style } from "@vanilla-extract/css";
 export const searchBar = style({
   padding: "8px 16px",
   display: "flex",
+  alignItems: "center",
   borderRadius: "4px",
   border: `1px solid ${theme.gray[100]}`,
   background: theme.white,
   gap: "8px",
   width: "320px",
+});
+
+export const input = style({
   ...font.p3,
+  width: "100%",
   "::placeholder": {
     color: theme.gray[300],
   },
