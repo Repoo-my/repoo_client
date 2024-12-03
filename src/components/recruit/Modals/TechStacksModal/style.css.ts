789067216efa590978fe3c98631028cacf3c7c74
추성우 <cntjddn715@gmail.com>
2024-12-03 20:17:46 +0900

    add :: 기술 스택 입력 모달

--- src/components/recruit/Modals/TechStacksModal/style.css.ts
+++ src/components/recruit/Modals/TechStacksModal/style.css.ts
@@ -30,6 +30,7 @@ export const title = style({
 export const stackList = style({
   display: "flex",
   gap: "8px",
+  flexWrap: "wrap",
 });
 
 export const stack = style({
@@ -40,4 +41,28 @@ export const stack = style({
   gap: "4px",
   ...font.p3,
   color: theme.gray[600],
+  borderRadius: "4px",
+});
+
+export const buttons = style({
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export const reset = style({
+  padding: "8px 14px",
+  background: theme.white,
+  color: theme.black,
+  borderRadius: "8px",
+  border: `1px solid ${theme.gray[100]}`,
+});
+
+export const reply = style({
+  padding: "8px 16px",
+  background: theme.gray[600],
+  borderRadius: "8px",
+  color: theme.white,
+  ...font.btn2,
 });
--- src/components/recruit/SearchBar/index.tsx
+++ src/components/recruit/SearchBar/index.tsx
@@ -5,13 +5,29 @@ import * as s from "./style.css";
 interface SearchBarProps {
   placeholder: string;
   width: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-function SearchBar({ placeholder, width }: SearchBarProps) {
+function SearchBar({
+  placeholder,
+  width,
+  value,
+  onChange,
+  onKeyPress,
+}: SearchBarProps) {
   return (
     <div className={s.searchBar} style={{ width }}>
       <Search />
-      <input type="text" placeholder={placeholder} className={s.input} />
+      <input
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        className={s.input}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
     </div>
   );
 }
