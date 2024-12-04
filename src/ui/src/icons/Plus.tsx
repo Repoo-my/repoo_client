6f57e7a34eae4fa1c2378d0cceadf9243a82a308
추성우 <cntjddn715@gmail.com>
2024-12-04 19:55:47 +0900

    add :: 맞춤공고 페이지 퍼블리싱

---
+++ src/ui/src/icons/Plus.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Plus() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icon / heroicons / Outline / plus">
+        <path
+          id="Icon"
+          d="M9.99998 3.33337V16.6667M16.6666 10L3.33331 10"
+          stroke="#787878"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default Plus;
