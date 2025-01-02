5e2aa534ba2cf2c8722492347f8b3c08db8f3d3c
추성우 <cntjddn715@gmail.com>
2025-01-02 17:22:27 +0900

    delete :: 로고 default 값

--- src/ui/src/assets/Logo.tsx
+++ src/ui/src/assets/Logo.tsx
@@ -5,7 +5,7 @@ interface LogoProps {
   height: number;
 }
 
-function Logo({ width = 104, height = 48 }: LogoProps) {
+function Logo({ width, height }: LogoProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
