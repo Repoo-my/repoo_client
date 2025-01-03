18ab15d8b1f640a23c297b5a31cae2947439700c
추성우 <cntjddn715@gmail.com>
2025-01-03 10:06:15 +0900

    chore :: 채용 공고 interface

---
+++ src/types/IJobpost.ts
@@ -0,0 +1,10 @@
+export interface IJobpost {
+  enterpriseName: string;
+  jobGroupName: string;
+  jobName: string;
+  maxStanding: number;
+  minStanding: number;
+  title: string;
+  contents: string;
+  jobPostImg: string;
+}
